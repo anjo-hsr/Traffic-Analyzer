3d3f1240fbc5715ed1e63b9390c932805268b1e4
AndiHSR <andi.hoerler@hsr.ch>
2019-03-22 14:09:38 +0100

    Restructured functions and variables

--- bin/get_mac.py
+++ bin/get_mac.py
@@ -2,23 +2,31 @@ import urllib.request
 import csv
 import re
 
-url = "http://standards-oui.ieee.org/oui/oui.csv"
 
-file_name = url.split("/")[-1]
+def is_header():
+    return line_counter == 0
 
-urllib.request.urlretrieve(url, file_name)
 
-mac_vendor = dict()
+def convert_mac_address():
+    vendor_part = row[1].lower()
+    mac_address_array = re.findall("..?", vendor_part)
+    return ":".join(mac_address_array)
+
+
+mac_vendor_url = "http://standards-oui.ieee.org/oui/oui.csv"
+file_name = mac_vendor_url.split("/")[-1]
+urllib.request.urlretrieve(mac_vendor_url, file_name)
+
 with \
-        open(file_name, mode="r", encoding='utf-8') as csv_file,\
+        open(file_name, mode="r", encoding='utf-8') as csv_file, \
         open('test2.csv', 'w', encoding='utf-8') as output_file:
-    output_file.write("eth_short,vendor\n")
+    mac_vendor = dict()
     csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
+
+    output_file.write("eth_short,vendor\n")
+    line_counter = 0
     for row in csv_reader:
-        if line_count > 0:
-            row[1] = row[1].lower()
-            mac_address_array = re.findall("..?", row[1])
-            mac_address = ":".join(mac_address_array)
+        if not is_header():
+            mac_address = convert_mac_address()
             output_file.write(mac_address + ',"' + row[2] + '"\n')
-        line_count += 1
+        line_counter += 1
