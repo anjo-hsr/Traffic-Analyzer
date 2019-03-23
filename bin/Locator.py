e49702ff2fa78c6f234fcc6973c0cf41634c876a
AndiHSR <andi.hoerler@hsr.ch>
2019-03-23 13:11:55 +0100

    Merged functions to add_information.py

---
+++ bin/Locator.py
@@ -0,0 +1,64 @@
+import socket
+import requests
+import json
+import time
+from bin import Limiter
+from bin.IPHelper import IPHelper
+
+
+class Locator:
+    def __init__(self):
+        self.locations = dict()
+        self.header = "dst_latitutde,dst_longitude,src_latitutde,src_longitude"
+
+    def check_request_load(self, limiter):
+        current_time = Limiter.get_current_timestamp()
+        limiter.increase_counter()
+        waiting_time = limiter.get_period_time() - (current_time - limiter.get_period_timestamp())
+
+        if (limiter.counter == limiter.requests_per_period) and waiting_time > 0:
+            time.sleep(waiting_time)
+            limiter.reset_period_timestamp()
+
+        if waiting_time < 0:
+            limiter.reset_period_timestamp()
+
+    def set_entry(self, ip_addr, lat_long):
+        self.locations[ip_addr] = lat_long
+
+    def get_location(self, limiter, ip_addr):
+        if ip_addr in self.locations:
+            return
+
+        if ip_addr == "" or not IPHelper.is_public_ip(ip_addr):
+            lat_long = ["", ""]
+            self.set_entry(ip_addr, lat_long)
+            return
+
+        self.check_request_load(limiter)
+
+        try:
+            lat_long = self.locate_ip(ip_addr)
+            self.set_entry(ip_addr, lat_long)
+        except socket.herror:
+            pass
+
+    def locate_ip(self, ip_addr):
+        search_url = "https://tools.keycdn.com/geo.json?host=" + ip_addr
+        response = requests.get(search_url)
+        response_json = json.loads(response.content)
+        geo = response_json["data"]["geo"]
+        lat_long = [geo["latitude"], geo["longitude"]]
+        return lat_long
+
+    def locate(self, dst_src):
+        destination = dst_src[0]
+        source = dst_src[1]
+
+        limiter = Limiter.Limiter(3, 1)
+        self.get_location(limiter, destination)
+        self.get_location(limiter, source)
+        pos_dest = ','.join('"{}"'.format(cell) for cell in self.locations.get(destination))
+        pos_src = ','.join('"{}"'.format(cell) for cell in self.locations.get(source))
+
+        return "{},{}".format(pos_dest, pos_src)
---
+++ bin/add_information.py
@@ -0,0 +1,44 @@
+from bin.Locator import Locator
+from bin.NameResolver import NameResolver
+import csv
+
+
+def is_header(line_number):
+    return line_number == 0
+
+
+def write_line(output_file, line):
+    output_file.write(line + "\n")
+
+
+def combine_information(joined_default_cells, header_fqdn, header_location):
+    return "{},{},{}".format(joined_default_cells, header_fqdn, header_location)
+
+
+def main():
+    locator = Locator()
+    name_resolver = NameResolver()
+
+    with \
+            open('test.csv', mode="r", encoding='utf-8') as capture, \
+            open('enriched.csv', 'w', encoding='utf-8') as output_file:
+        csv_reader = csv.reader(capture, delimiter=',')
+        for index, packet in enumerate(csv_reader):
+            joined_default_cells = ','.join('"{}"'.format(cell) for cell in packet)
+            if is_header(index):
+                header_fqdn = name_resolver.header
+                header_location = locator.header
+                line = combine_information(joined_default_cells, header_fqdn, header_location)
+
+            else:
+                dst_ip_addr = packet[4]
+                src_ip_addr = packet[5]
+                src_dst = [dst_ip_addr, src_ip_addr]
+                location_information = locator.locate(src_dst)
+                fqdn_information = name_resolver.resolve(src_dst)
+                line = combine_information(joined_default_cells, fqdn_information, location_information)
+
+            write_line(output_file, line)
+
+
+main()
