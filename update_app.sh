173ec0cc39ec8e4113920285d0b40115cffb1a5c
Jonas Kugler <jonas.kugler@hsr.ch>
2019-04-03 08:58:21 +0200

    Change name of .tar file

--- update_app.sh
+++ update_app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 tar --exclude="./docker" --exclude="bin/files" --exclude="*.gitignore" --exclude="bin/test" --exclude="*/.*" --exclude="*/__pycache__" \
-    -zcvf ./docker/init_files/traffic_analyzer/traffic-analyzer.tar.gz \
+    -zcvf ./docker/init_files/traffic_analyzer/traffic_analyzer.tar.gz \
     ./appserver ./bin ./default ./local ./lookups ./metadata ./static \
     --transform s/./traffic_analyzer/
 
-docker cp docker/splunk_apps/traffic-analyzer.tar.gz splunk:/tmp/splunk_apps/
-docker exec -it splunk /opt/splunkt/bin/splunk install app /tmp/splunk_apps/traffic-analyzer.tar.gz -auth admin:AnJo-HSR -update 1
+docker cp docker/init_files/traffic_analyzer/traffic_analyzer.tar.gz splunk:/tmp/traffic_analyzer/
+docker exec -it splunk sudo /opt/splunk/bin/splunk install app /tmp/traffic_analyzer/traffic_analyzer.tar.gz -auth admin:AnJo-HSR -update 1
