4665e6c631ec690175ee82590b859918285e36c8
AndiHSR <andi.hoerler@hsr.ch>
2019-04-03 19:56:47 +0200

    Rewrite test by using dict

--- backend/bin/test/test_get_mac.py
+++ backend/bin/test/test_get_mac.py
@@ -6,11 +6,14 @@ import main.get_mac as get_mac
 class TestGetMacMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.mac_line = ["","FFFFFF"]
+        cls.line = {
+            "Assignment": "FFFFFF",
+            "Organization Name": "Broadcast Corp"
+        }
         cls.vendor_mac = "ff:ff:ff"
 
     def test_convert_to_vendor_mac(self):
-        self.assertEqual(get_mac.convert_mac_address(self.mac_line), self.vendor_mac)
+        self.assertEqual(get_mac.convert_mac_address(self.line), self.vendor_mac)
 
 
 if __name__ == "__main__":
