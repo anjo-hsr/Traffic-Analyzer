48fc120fc3dd56dedbd25d98ae0196f6b8059257
AndiHSR <andi.hoerler@hsr.ch>
2019-04-03 17:47:43 +0200

    Expand environment tests

--- backend/bin/test/test_environment.py
+++ backend/bin/test/test_environment.py
@@ -8,13 +8,21 @@ from main.helpers.Environment import Environment
 class TestCombinerMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.environment_variables = {
+        cls.development_variables = {
+            "pcap_path": path.join("/tmp", "pcaps"),
+            "csv_path": path.join("/tmp", "csvs")
+        }
+
+        cls.production_variables = {
             "pcap_path": path.join("..", "..", "..", "docker", "init_files", "pcaps"),
             "csv_path": path.join("..", "files")
         }
 
-    def test_environment_test(self):
-        self.assertEqual(Environment.get_environment(), self.environment_variables)
+    def test_development_variables(self):
+        self.assertEqual(Environment.get_environment(), self.development_variables)
+
+    def test_production_variables(self):
+        self.assertEqual(Environment.get_environment(True), self.production_variables)
 
 
 if __name__ == "__main__":
