ab0abe00738696ab01d5be4c87a3566185e9bd5a
Jonas Kugler <jonas.kugler@hsr.ch>
2019-04-03 09:23:50 +0200

    Fix exec command in update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -6,4 +6,5 @@ tar --exclude="./docker" --exclude="bin/files" --exclude="*.gitignore" --exclude
     --transform s/./traffic_analyzer/
 
 docker cp docker/init_files/traffic_analyzer/traffic_analyzer.tar.gz splunk:/tmp/traffic_analyzer/
-docker exec -it splunk sudo /opt/splunk/bin/splunk install app /tmp/traffic_analyzer/traffic_analyzer.tar.gz -auth admin:AnJo-HSR -update 1
+docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk install app /tmp/traffic_analyzer/traffic_analyzer.tar.gz -auth admin:AnJo-HSR -update 1'
+docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk restart splunkd'
