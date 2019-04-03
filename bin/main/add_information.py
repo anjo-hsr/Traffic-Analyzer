3de384119381c3f041ce83dc721df8ea7f76b91f
AndiHSR <andi.hoerler@hsr.ch>
2019-04-03 10:44:41 +0200

    Extract methods and remove not needed imports

--- bin/main/add_information.py
+++ bin/main/add_information.py
@@ -1,7 +1,6 @@
 from bin.main.helpers.Environment import Environment
 from bin.main.helpers.Locator import Locator
 from bin.main.helpers.NameResolver import NameResolver
-from bin.main.helpers.Timer import Timer
 from bin.main.helpers.Combiner import Combiner
 from bin.main.helpers.CipherSuites import CipherSuites
 
@@ -46,24 +45,31 @@ def run(environment_variables):
 
     for (dirpath, dirnames, filenames) in walk(csv_path):
         for file in filenames:
-            helpers = {"locator": Locator(), "name_resolver": NameResolver(), "cipher_suites": CipherSuites()}
+            helpers = create_helpers()
 
             if is_normal_csv_file(file):
                 new_file = re.sub(".csv$", "-enriched.csv", str(file))
-
-                with \
-                        open(path.join(dirpath, file), mode="r", encoding='utf-8') as capture, \
-                        open(path.join(dirpath, new_file), 'w', encoding='utf-8') as output_file:
-                    csv_delimiter = ","
-                    csv_reader = csv.DictReader(capture, delimiter=csv_delimiter)
-
-                    loop_through_lines(csv_delimiter, csv_reader, helpers, output_file)
-
-                    print_dicts(helpers)
+                enrich_file(dirpath, file, helpers, new_file)
 
 
 def is_normal_csv_file(file):
     return str(file).startswith("capture") and str(file).endswith(".csv") and not str(file).endswith("-enriched.csv")
+
+
+def create_helpers():
+    return {"locator": Locator(), "name_resolver": NameResolver(), "cipher_suites": CipherSuites()}
+
+
+def enrich_file(dirpath, file, helpers, new_file):
+    with \
+            open(path.join(dirpath, file), mode="r", encoding='utf-8') as capture, \
+            open(path.join(dirpath, new_file), 'w', encoding='utf-8') as output_file:
+        csv_delimiter = ","
+        csv_reader = csv.DictReader(capture, delimiter=csv_delimiter)
+
+        loop_through_lines(csv_delimiter, csv_reader, helpers, output_file)
+
+        print_dicts(helpers)
 
 
 main()
