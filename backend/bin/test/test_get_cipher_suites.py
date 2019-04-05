5d5258142f2f66ebaccd6afa3eb5c092486c9cfc
AndiHSR <andi.hoerler@hsr.ch>
2019-04-05 12:33:24 +0200

    Add test to write row

--- backend/bin/test/test_get_cipher_suites.py
+++ backend/bin/test/test_get_cipher_suites.py
@@ -1,9 +1,20 @@
 import unittest
 
+from os import path, remove
+
 import main.downloaders.get_ciphers as get_ciphers
 
 
 class TestGetCiphersMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.row_dict = {
+            "Value": "0xC0, 0x30",
+            "Description": "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+            "Recommended": "Y",
+        }
+        cls.expected_line = "49200,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,Y"
+
     def test_calculate_hex(self):
         hex_dict = {
             "0x00,0x20": 32,
@@ -13,15 +24,30 @@ class TestGetCiphersMethods(unittest.TestCase):
             self.assertEqual(get_ciphers.calculate_hex(hex_line), hex_dict[hex_line])
 
     def test_combine_information(self):
-        row_dict = {
-            "Value": "0xC0, 0x30",
-            "Description": "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-            "Recommended": "Y",
-        }
-        expected_line = "49200,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,Y"
+        actual_line = get_ciphers.combine_information(self.row_dict)
+        self.assertEqual(actual_line, self.expected_line)
 
-        actual_line = get_ciphers.combine_information(row_dict)
-        self.assertEqual(actual_line, expected_line)
+    def test_write_row_successful(self):
+        test_file_path = path.join(".", "test.csv")
+        with open(test_file_path, mode="w") as test_file:
+            get_ciphers.write_row(test_file, self.row_dict)
+
+        with open(test_file_path) as test_file:
+            self.assertEqual(test_file.read(), self.expected_line + "\n")
+
+        remove(test_file_path)
+
+    def test_write_row_failing(self):
+        test_file_path = path.join(".", "test.csv")
+        row_dict = self.row_dict.copy()
+        row_dict["Value"] = "0xC0, 0x2F-30"
+        with open(test_file_path, mode="w") as test_file:
+            get_ciphers.write_row(test_file, row_dict)
+
+        with open(test_file_path) as test_file:
+            self.assertEqual(test_file.read(), "")
+
+        remove(test_file_path)
 
 
 if __name__ == "__main__":
