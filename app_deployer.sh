3889c9c9dd9e82ce5b7e5197c86050e5d8e9bcac
AndiHSR <andi.hoerler@hsr.ch>
2019-04-07 09:01:31 +0200

    Rename app based on repository name and change deployment procedure

--- app_deployer.sh
+++ app_deployer.sh
@@ -1,78 +1,90 @@
 #!/bin/bash
 
 function stop_splunk(){
-    echo -e "Stop all running splunk containers\n"
-    docker stop $(docker ps -af "name=splunk")
-    echo -e "\nContainers stopped\n-----------\nStart deleting splunk containers\n"
+    echo -e "\nStop all running $containerName containers\n"
+    docker stop $(docker ps -af "name=$containerName")
+    echo -e "\nContainers stopped\n-----------\nStart deleting $containerName containers\n"
 }
 
 function delete_containers() {
-    docker rm $(docker ps -af "name=splunk")
-    echo -e "\nContainers deleted\n-----------\nStart deleting all splunk_deployment images\n"
-    docker rmi splunk_deployment
+    docker rm $(docker ps -af "name=$containerName")
+    echo -e "\nContainers deleted\n-----------\nStart deleting all $imageName images\n"
+    docker rmi ${imageName}
     echo -e "\nImages deleted\n"
 }
 
 function building_splunk() {
-    echo -e "Building with docker-compose"
+    echo -e "\nBuilding with docker-compose"
     docker-compose up -d
     echo -e "Wait till docker container is running (~60s)"
 
-    while [[ $(docker ps -q --filter health=starting --filter name=splunk --format "{{.Names}}" | grep splunk) ]]
+    while [[ $(docker ps -q --filter health=starting --filter name=${containerName} --format "{{.Names}}" | grep ${containerName}) ]]
     do
         sleep 1
     done
 }
 
 function create_tar() {
-    echo -e "Create traffic_analyzer.tar.gz"
+    echo -e "\nCreate traffic-analyzer.tar.gz"
     tar --exclude="./docker" --exclude="bin/files" --exclude="*.gitignore" --exclude="bin/test" \
         --exclude="*/.*" --exclude="*/__pycache__" \
-        -zcvf ./docker/init_files/traffic_analyzer/traffic_analyzer.tar.gz \
+        -zcvf ./docker/init_files/traffic-analyzer/traffic-analyzer.tar.gz \
         -C backend/ bin \
         -C ../frontend/ appserver default local lookups metadata static \
-        --transform "s,^,traffic_analyzer/,"
+        --transform "s,^,traffic-analyzer/,"
 
-    echo -e "File traffic_analyzer.tar.gz file created"
+    echo -e "File traffic-analyzer.tar.gz file created"
 }
 
-function remove_traffic_analyzer() {
-    echo -e "Remove traffic_analyzer from docker container"
-    docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk remove app traffic_analyzer -auth admin:AnJo-HSR'
+function remove_traffic-analyzer() {
+    echo -e "\nRemove traffic-analyzer from docker container"
+    docker exec ${containerName} bash -c 'sudo /opt/splunk/bin/splunk remove app traffic-analyzer -auth admin:AnJo-HSR'
 }
 
-function update_traffic_analyzer() {
-    echo -e "Copy traffic_analyzer.tar.gz to docker container and restart splunk service"
+function install_requirements() {
+    echo -e "\nCopy requirements inside requirements.txt and install them"
+    docker cp ./requirements.txt ${containerName}:/tmp/
+    docker exec ${containerName} bash -c 'sudo pip3 install -r /tmp/requirements.txt'
+}
+
+function update_traffic-analyzer() {
+    echo -e "\nCopy traffic-analyzer.tar.gz to docker container and restart splunk service"
     sleep 5
-    docker cp docker/init_files/traffic_analyzer/traffic_analyzer.tar.gz splunk:/tmp/traffic_analyzer/
-    docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk install app /tmp/traffic_analyzer/traffic_analyzer.tar.gz -auth admin:AnJo-HSR -update 1'
-    docker exec splunk bash -c 'sudo /opt/splunk/bin/splunk restart splunkd'
+    docker cp ./docker/init_files/traffic-analyzer/traffic-analyzer.tar.gz ${containerName}:/tmp/traffic-analyzer/
+    docker exec ${containerName} bash -c 'sudo /opt/splunk/bin/splunk install app /tmp/traffic-analyzer/traffic-analyzer.tar.gz -auth admin:AnJo-HSR -update 1'
+    docker exec ${containerName} bash -c 'sudo /opt/splunk/bin/splunk restart splunkd'
 }
 
 function import_csvs() {
-    echo -e "Import standard csv files from ./docker/init_files/csv"
-    docker exec splunk bash -c 'for csv in /tmp/csv/*.csv; do sudo /opt/splunk/bin/splunk add oneshot "$csv" -auth admin:AnJo-HSR; done'
+    echo -e "\nImport standard csv files from ./docker/init_files/csv"
+    docker exec ${containerName} bash -c 'for csv in /tmp/csv/*.csv; do sudo /opt/splunk/bin/splunk add oneshot "$csv" -auth admin:AnJo-HSR; done'
 }
+
+containerName="splunk_traffic-analyzer"
+imageName="${containerName}_image"
 
 case "$1" in
     start)
         stop_splunk
         delete_containers
         building_splunk
+        install_requirements
         create_tar
-        update_traffic_analyzer
+        update_traffic-analyzer
         import_csvs
         ;;
 
     update)
+        install_requirements
         create_tar
-        update_traffic_analyzer
+        update_traffic-analyzer
         ;;
 
     force-update)
+        install_requirements
         create_tar
-        remove_traffic_analyzer
-        update_traffic_analyzer
+        remove_traffic-analyzer
+        update_traffic-analyzer
         ;;
 
     *)
