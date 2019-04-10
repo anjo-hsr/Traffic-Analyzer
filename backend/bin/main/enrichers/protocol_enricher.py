eafb0e9e413e7289dedc20ea67f00f273d360ad6
AndiHSR <andi.hoerler@hsr.ch>
2019-04-10 19:02:30 +0200

    Add header for class

--- backend/bin/main/enrichers/protocol_enricher.py
+++ backend/bin/main/enrichers/protocol_enricher.py
@@ -1,5 +1,8 @@
 class ProtocolEnricher:
+    def __init__(self):
+        self.header = "protocol"
+
     def get_protocol(self, packet):
         protocols = packet["frame.protocols"]
         protocol_list = protocols.split(":")
-        return protocol_list[len(protocol_list)-1]
+        return protocol_list[len(protocol_list) - 1]
