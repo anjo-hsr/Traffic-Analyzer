5dfad1b347c5ab445258241574b32ba7f58b0256
AndiHSR <andi.hoerler@hsr.ch>
2019-04-10 19:23:56 +0200

    Add print method for protocol_enricher

--- backend/bin/main/enrichers/protocol_enricher.py
+++ backend/bin/main/enrichers/protocol_enricher.py
@@ -1,8 +1,21 @@
+from main.helpers.print_helper import PrintHelper
+
+
 class ProtocolEnricher:
     def __init__(self):
         self.header = "protocol"
+        self.protocols = set([])
 
     def get_protocol(self, packet):
         protocols = packet["frame.protocols"]
         protocol_list = protocols.split(":")
-        return protocol_list[len(protocol_list) - 1]
+        protocol = protocol_list[len(protocol_list) - 1]
+
+        if protocol != "":
+            self.protocols.add(protocol)
+
+        return protocol
+
+    def print(self):
+        print_text = "Print out for all {} protocols"
+        PrintHelper.print_list(self.protocols, print_text)
