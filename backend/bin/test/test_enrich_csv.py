3f4e281d0dc8c8c7d3b4ed756547feb7ac68d22d
AndiHSR <andi.hoerler@hsr.ch>
2019-04-10 17:50:51 +0200

    Update tests

--- backend/bin/test/test_enrich_csv.py
+++ backend/bin/test/test_enrich_csv.py
@@ -1,9 +1,10 @@
 import unittest
 
 import main.enrich_csv as add_information
+from main.enrichers.cipher_suite_enricher import CipherSuiteEnricher
 from main.enrichers.location_enricher import LocationEnricher
 from main.enrichers.name_resolve_enricher import NameResolverEnricher
-from main.enrichers.cipher_suite_enricher import CipherSuiteEnricher
+from main.enrichers.protocol_enricher import ProtocolEnricher
 from main.enrichers.tls_enricher import TlsEnricher
 
 
@@ -13,17 +14,20 @@ class TestEnrichCsv(unittest.TestCase):
         keys = helpers.keys()
 
         for key in keys:
-            if key == "locator":
+            if key == "location_enricher":
                 self.assertTrue(isinstance(helpers[key], LocationEnricher))
 
-            elif key == "name_resolver":
+            elif key == "name_resolve_enricher":
                 self.assertTrue(isinstance(helpers[key], NameResolverEnricher))
 
-            elif key == "cipher_suites":
+            elif key == "cipher_suite_enricher":
                 self.assertTrue(isinstance(helpers[key], CipherSuiteEnricher))
 
-            elif key == "tls_ssl_version":
+            elif key == "tls_ssl_version_enricher":
                 self.assertTrue(isinstance(helpers[key], TlsEnricher))
+
+            elif key == "protocol_enricher":
+                self.assertTrue(isinstance(helpers[key], ProtocolEnricher))
 
             else:
                 self.assertTrue(False)
@@ -33,7 +37,7 @@ class TestEnrichCsv(unittest.TestCase):
         self.assertTrue(isinstance(helpers, dict))
 
     def test_create_helpers_keys(self):
-        test_keys = ["locator", "name_resolver", "cipher_suites", "tls_ssl_version"]
+        test_keys = ["location_enricher", "name_resolve_enricher", "cipher_suite_enricher", "tls_ssl_version_enricher", "protocol_enricher"]
 
         helpers = add_information.create_enrichers()
         keys = [helper_key for helper_key in helpers]
