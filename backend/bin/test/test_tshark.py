2573b628216ef97aace2144e956e55bbc943df19
AndiHSR <andi.hoerler@hsr.ch>
2019-04-10 12:28:14 +0200

    Fix test for argumnets from tshark

--- backend/bin/test/test_tshark.py
+++ backend/bin/test/test_tshark.py
@@ -6,14 +6,12 @@ import main.helpers.Tshark as Tshark
 class TestTsharkMethods(unittest.TestCase):
     def test_tshark_get_arguments(self):
         filename = "test.pcap"
-        args = ['-r', filename, "-T", "fields", "-e", "frame.time",
-                "-e", "frame.cap_len", "-e", "eth.dst", "-e", "eth.src",
-                "-e", "ip.dst", "-e", "ip.src", "-e", "ip.proto",
-                "-e", "tcp.srcport", "-e", "tcp.dstport", "-e", "tcp.flags", "-e", "tcp.len", "-e", "tcp.stream",
-                "-e", "udp.srcport", "-e", "udp.dstport", "-e", "udp.length",
-                "-e", "http.request.method", "-e", "http.request.uri",
-                "-e", "tls.record.version", "-e", "tls.handshake.ciphersuite", "-e", "tls.handshake.ciphersuites",
-                "-E", "header=y", "-E", "separator=,", "-E", "quote=d", "-E", "occurrence=f"]
+        args = ["-r", filename, "-T", "fields", "-e", "frame.time", "-e", "frame.cap_len", "-e", "eth.dst", "-e",
+                "eth.src", "-e", "ip.dst", "-e", "ip.src", "-e", "ip.proto", "-e", "tcp.srcport", "-e", "tcp.dstport",
+                "-e", "tcp.flags", "-e", "tcp.len", "-e", "tcp.stream", "-e", "udp.srcport", "-e", "udp.dstport", "-e",
+                "udp.length", "-e", "http.request.method", "-e", "http.request.uri", "-e", "tls.handshake.version",
+                "-e", "tls.handshake.extensions.supported_version", "-e", "tls.handshake.ciphersuite", "-e",
+                "tls.handshake.type", "-E", "header=y", "-E", "separator=,", "-E", "quote=d", "-E", "occurrence=f"]
 
         self.assertEqual(Tshark.get_arguments(filename), args)
 
