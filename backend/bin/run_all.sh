07ef4e9d72da5c7996f189e89eb2efd79fb3c490
AndiHSR <andi.hoerler@hsr.ch>
2019-04-12 20:00:10 +0200

    Redirect output to /dev/null to prevent indexing

--- backend/bin/run_all.sh
+++ backend/bin/run_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ${SPLUNK_HOME}/etc/apps/traffic-analyzer/bin/main/traffic_analyzer.py run-all
+python3 ${SPLUNK_HOME}/etc/apps/traffic-analyzer/bin/main/traffic_analyzer.py run-all &>/dev/null
