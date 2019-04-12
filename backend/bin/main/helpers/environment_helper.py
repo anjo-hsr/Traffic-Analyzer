1125d298044ab0221fda638b2664ceff2faadcdf
AndiHSR <andi.hoerler@hsr.ch>
2019-04-12 21:43:35 +0200

    Use process splunkd to check if it's in production

--- backend/bin/main/helpers/environment_helper.py
+++ backend/bin/main/helpers/environment_helper.py
@@ -1,14 +1,17 @@
-from os import environ, path
+from os import path
+
+import psutil
 
 
 class EnvironmentHelper:
 
     def __init__(self):
-        self.production_env_identifier = "SPLUNK_HOME"
+        self.production_process_name = "splunkd"
         self.splunk_app_folder = path.join("/opt", "splunk", "etc", "apps", "traffic-analyzer")
 
     def get_environment(self):
-        is_splunk_server = environ.get(self.production_env_identifier) is not None
+        is_splunk_server = self.check_process(self.production_process_name)
+
         environment_variables = {}
 
         if is_splunk_server:
@@ -20,9 +23,16 @@ class EnvironmentHelper:
 
         else:
             environment_variables["pcap_path"] = path.join("..", "..", "..", "docker", "init_files", "pcaps")
-            environment_variables["pcap_processed_path_path"] = path.join("..", "..", "..", "docker", "init_files", "pcaps")
+            environment_variables["pcap_processed_path"] = path.join("..", "..", "..", "docker", "init_files",
+                                                                          "pcaps")
             environment_variables["csv_tmp_path"] = path.join("..", "files")
             environment_variables["csv_capture_path"] = path.join("..", "files")
             environment_variables["csv_list_path"] = path.join("..", "files")
 
         return environment_variables
+
+    def check_process(self, process_name):
+        for process in psutil.process_iter(attrs=["name"]):
+            if process.info["name"] == process_name:
+                return True
+        return False
--- backend/bin/test/test_helpers/test_environment_helper.py
+++ backend/bin/test/test_helpers/test_environment_helper.py
@@ -6,6 +6,11 @@ from unittest.mock import patch
 from main.helpers.environment_helper import EnvironmentHelper
 
 
+class Process():
+    def __init__(self, name):
+        self.info = {"name": name}
+
+
 class TestEnrivonmentHelperMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -13,23 +18,29 @@ class TestEnrivonmentHelperMethods(unittest.TestCase):
 
         cls.development_variables = {
             "pcap_path": path.join("..", "..", "..", "docker", "init_files", "pcaps"),
+            "pcap_processed_path": path.join("..", "..", "..", "docker", "init_files", "pcaps"),
             "csv_tmp_path": path.join("..", "files"),
             "csv_list_path": path.join("..", "files"),
             "csv_capture_path": path.join("..", "files")
         }
         cls.production_variables = {
             "pcap_path": path.join("/tmp", "pcaps"),
+            "pcap_processed_path": path.join("/tmp", "pcaps_processed"),
             "csv_tmp_path": path.join("/tmp", "csvs"),
             "csv_list_path": path.join("/opt", "splunk", "etc", "apps", "traffic-analyzer", "lookups", "lists"),
             "csv_capture_path": path.join("/opt", "splunk", "etc", "apps", "traffic-analyzer", "lookups", "captures")
         }
 
-    @patch.dict("os.environ", {})
-    def test_development_variables(self):
+    @patch("psutil.process_iter")
+    def test_development_variables(self, process_iter):
+        process = Process("dev_test")
+        process_iter.return_value = [process]
         self.assertEqual(self.environment_helper.get_environment(), self.development_variables)
 
-    @patch.dict("os.environ", {"SPLUNK_HOME": "/opt/splunk"})
-    def test_production_variables(self):
+    @patch("psutil.process_iter")
+    def test_production_variables(self, process_iter):
+        process = Process("splunkd")
+        process_iter.return_value = [process]
         self.assertEqual(self.environment_helper.get_environment(), self.production_variables)
 
 
