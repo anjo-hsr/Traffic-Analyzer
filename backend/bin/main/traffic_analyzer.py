fab7ffcb9d7015f8e4417772df5e9e34c745e3f6
AndiHSR <andi.hoerler@hsr.ch>
2019-04-12 08:04:05 +0200

    Add new run method and print help as default

--- backend/bin/main/traffic_analyzer.py
+++ backend/bin/main/traffic_analyzer.py
@@ -28,6 +28,11 @@ def enrich():
 
 
 def run():
+    convert()
+    enrich()
+
+
+def run_all():
     download()
     convert()
     enrich()
@@ -44,8 +49,18 @@ def main():
         enrich()
     elif method == "run":
         run()
+    elif method == "run-all":
+        run_all()
     else:
-        print("Please use (convert|enrich|run)")
+        print(
+            "Usage traffic_analyzer.py [option]\n\n"
+            "Option:\n"
+            "   download:    Download information from IANA and IEEE.\n"
+            "   convert:     Converts pcap(ng) files to csv\n"
+            "   enrich:      Enriches csvs with additional information\n"
+            "   run:         Runs convert and enrich\n"
+            "   run-all:     Runs download, convert and enrich"
+        )
 
 
 if __name__ == "__main__":
