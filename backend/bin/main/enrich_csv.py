f4dbec2c1cfa2e84bbd6d0dd9fd6aaea372a6108
AndiHSR <andi.hoerler@hsr.ch>
2019-04-13 17:51:11 +0200

    Remove unneeded pass

--- backend/bin/main/enrich_csv.py
+++ backend/bin/main/enrich_csv.py
@@ -70,7 +70,6 @@ def run(environment_variables):
     for dirpath, _, filenames in walk(csv_tmp_path):
         for file in filenames:
             if file_helper.is_enriched_csv_file(file):
-                pass
                 file_helper.move_file(
                     path.join(dirpath, file),
                     path.join(csv_capture_path, file)
