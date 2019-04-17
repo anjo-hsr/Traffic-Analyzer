c7d21f9dde86581a59940264adbe4f351ded6222
AndiHSR <andi.hoerler@hsr.ch>
2019-04-17 16:25:24 +0200

    Fix enricher tests

--- backend/bin/test/test_enrich_csv.py
+++ backend/bin/test/test_enrich_csv.py
@@ -2,6 +2,7 @@ import unittest
 from collections import OrderedDict
 
 import main.enrich_csv as add_information
+from main.enrichers.ad_enricher import AdEnricher
 from main.enrichers.cipher_suite_enricher import CipherSuiteEnricher
 from main.enrichers.location_enricher import LocationEnricher
 from main.enrichers.name_resolve_enricher import NameResolverEnricher
@@ -26,6 +27,9 @@ class TestEnrichCsv(unittest.TestCase):
             elif key == "tls_ssl_version_enricher":
                 self.assertTrue(isinstance(enrichers[key], TlsEnricher))
 
+            elif key == "ad_enricher":
+                self.assertTrue(isinstance(enrichers[key], AdEnricher))
+
             else:
                 self.assertTrue(False)
 
@@ -34,7 +38,13 @@ class TestEnrichCsv(unittest.TestCase):
         self.assertTrue(isinstance(enrichers, OrderedDict))
 
     def test_create_enrichers_keys(self):
-        test_keys = ["location_enricher", "name_resolve_enricher", "cipher_suite_enricher", "tls_ssl_version_enricher"]
+        test_keys = [
+            "location_enricher",
+            "name_resolve_enricher",
+            "cipher_suite_enricher",
+            "tls_ssl_version_enricher",
+            "ad_enricher"
+        ]
 
         enrichers = add_information.create_enrichers()
         keys = [enricher_key for enricher_key in enrichers]
