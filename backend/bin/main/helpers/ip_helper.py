33bfabc033c5597fc1e9f7420142ac0df4bad0f2
AndiHSR <andi.hoerler@hsr.ch>
2019-04-24 15:26:28 +0200

    Add double check for multicast addresses

--- backend/bin/main/helpers/ip_helper.py
+++ backend/bin/main/helpers/ip_helper.py
@@ -5,7 +5,11 @@ class IpHelper:
     @staticmethod
     def is_public_ip(ip_addr):
         try:
-            return ipaddress.ip_address(ip_addr).is_global
+            # Double check must be made because ipaddress classifies 224.0.0.1 as a global address which would then be
+            # searched over nslookup.
+            # https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml#multicast-addresses-1
+            return ipaddress.ip_address(ip_addr).is_global and \
+                   not ipaddress.ip_address(ip_addr).is_multicast
         except ValueError:
             return False
 
