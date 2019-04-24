faf2ecc1bb9bf5d2c64918fe3eddc20a976602fe
Jonas Kugler <jonas.kugler@hsr.ch>
2019-04-24 15:00:06 +0200

    Add javascript to store tokens

---
+++ frontend/appserver/static/tokenStorer.js
@@ -0,0 +1,25 @@
+require([
+    "splunkjs/mvc",
+    "splunkjs/mvc/simplexml/ready!"
+], function (
+    mvc
+) {
+    let defaultTokenModel = mvc.Components.get("default");
+
+    let savedTokens = window.sessionStorage;
+    Object.keys(savedTokens).forEach(tokenKey => {
+        let currentToken = defaultTokenModel.get(tokenKey);
+        if (Boolean(currentToken)) {
+            defaultTokenModel.set(tokenKey, savedTokens[tokenKey]);
+        }
+    });
+
+    defaultTokenModel.on("change", function () {
+        let attributes = defaultTokenModel.attributes;
+        Object.keys(attributes).forEach(tokenKey =>
+            window.sessionStorage.setItem(tokenKey, attributes[tokenKey])
+        );
+    });
+
+
+});
