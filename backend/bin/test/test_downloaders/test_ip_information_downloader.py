94c09da70342402cccbdd12d2538951077b38c1e
AndiHSR <andi.hoerler@hsr.ch>
2019-04-28 20:37:39 +0200

    Fix and add test

--- backend/bin/test/test_downloaders/test_ip_information_downloader.py
+++ backend/bin/test/test_downloaders/test_ip_information_downloader.py
@@ -1,15 +1,25 @@
 import unittest
 
 from main.downloaders.ip_information_downloader import IpInformationDownloader
+from main.helpers.ip_helper import IpHelper
 
 
 class TestIpInformationDownloader(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.private_ip = "10.0.0.1"
+        cls.multicast_ip = "224.0.0.1"
         cls.public_ip = "8.8.8.8"
-        cls.empty_ip_data = {
+        cls.ip_data_private = {
             "rdns": cls.private_ip,
+            "asn": "",
+            "isp": "",
+            "latitude": "",
+            "longitude": ""
+        }
+
+        cls.ip_data_multicast = {
+            "rdns": cls.multicast_ip,
             "asn": "",
             "isp": "",
             "latitude": "",
@@ -29,7 +39,7 @@ class TestIpInformationDownloader(unittest.TestCase):
 
     def test_get_ip_information_private(self):
         ip_address = self.private_ip
-        self.assertDictEqual(self.ip_information_downloader.get_ip_information(ip_address), self.empty_ip_data)
+        self.assertDictEqual(self.ip_information_downloader.get_ip_information(ip_address), self.ip_data_private)
 
     def test_get_ip_data(self):
         ip_address = self.public_ip
@@ -38,15 +48,21 @@ class TestIpInformationDownloader(unittest.TestCase):
     def test_get_ip_information_twice(self):
         ip_address = self.private_ip
         length = 1
-        self.assertEqual(self.ip_information_downloader.get_ip_information(ip_address), self.empty_ip_data)
+        self.assertEqual(self.ip_information_downloader.get_ip_information(ip_address), self.ip_data_private)
         self.assertEqual(len(self.ip_information_downloader.ip_information), length)
 
-        self.assertEqual(self.ip_information_downloader.get_ip_information(ip_address), self.empty_ip_data)
+        self.assertEqual(self.ip_information_downloader.get_ip_information(ip_address), self.ip_data_private)
         self.assertEqual(len(self.ip_information_downloader.ip_information), length)
 
-    def test_get_empty_ip_data(self):
+    def test_get_private_ip_data_multicast(self):
+        ip_address = self.multicast_ip
+        ip_helper = IpHelper()
+        self.assertEqual(IpInformationDownloader.get_private_ip_data(ip_address, ip_helper), self.ip_data_multicast)
+
+    def test_get_private_ip_data_private(self):
         ip_address = self.private_ip
-        self.assertEqual(IpInformationDownloader.get_empty_ip_data(ip_address), self.empty_ip_data)
+        ip_helper = IpHelper()
+        self.assertEqual(IpInformationDownloader.get_private_ip_data(ip_address, ip_helper), self.ip_data_private)
 
 
 if __name__ == "__main__":
