fc6a5de0e8968538d5c47a70d0b3e67aa68b07ab
Jonas Kugler <jonas.kugler@hsr.ch>
2019-05-10 09:39:38 +0200

    Add custom app setup

---
+++ backend/bin/conf/endpoint_handler.py
@@ -0,0 +1,77 @@
+import splunk.admin as admin
+import splunk.entity as en
+
+# import your required python modules
+
+'''
+Copyright (C) 2005 - 2010 Splunk Inc. All Rights Reserved.
+Description:  This skeleton python script handles the parameters in the configuration page.
+
+      handleList method: lists configurable parameters in the configuration page
+      corresponds to handleractions = list in restmap.conf
+
+      handleEdit method: controls the parameters and saves the values 
+      corresponds to handleractions = edit in restmap.conf
+
+'''
+
+
+class ConfigApp(admin.MConfigHandler):
+    '''
+    Set up supported arguments
+    '''
+
+    def setup(self):
+        if self.requestedAction == admin.ACTION_EDIT:
+            for arg in ['safe_browsing_api_key']:
+                self.supportedArgs.addOptArg(arg)
+
+    '''
+    Read the initial values of the parameters from the custom file
+        myappsetup.conf, and write them to the setup page. 
+  
+    If the app has never been set up,
+        uses .../app_name/default/myappsetup.conf. 
+  
+    If app has been set up, looks at 
+        .../local/myappsetup.conf first, then looks at 
+    .../default/myappsetup.conf only if there is no value for a field in
+        .../local/myappsetup.conf
+  
+    For boolean fields, may need to switch the true/false setting.
+  
+    For text fields, if the conf file says None, set to the empty string.
+    '''
+
+    def handleList(self, confInfo):
+        confDict = self.readConf("traffic-analyzer")
+        if None != confDict:
+            for stanza, settings in confDict.items():
+                for key, val in settings.items():
+                    if key in ['safe_browsing_api_key'] and val in [None, '']:
+                        val = ''
+                    confInfo[stanza].append(key, val)
+
+    '''
+    After user clicks Save on setup page, take updated parameters,
+    normalize them, and save them somewhere
+    '''
+
+    def handleEdit(self, confInfo):
+        name = self.callerArgs.id
+        args = self.callerArgs
+
+        if self.callerArgs.data['safe_browsing_api_key'][0] in [None, '']:
+            self.callerArgs.data['safe_browsing_api_key'][0] = ''
+
+        '''
+        Since we are using a conf file to store parameters, 
+    write them to the [setupentity] stanza
+        in app_name/local/myappsetup.conf  
+        '''
+
+        self.writeConf('traffic-analyzer', 'Safe Browsing', self.callerArgs.data)
+
+
+# initialize the handler
+admin.init(ConfigApp, admin.CONTEXT_NONE)
