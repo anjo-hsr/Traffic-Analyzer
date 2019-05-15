5ffaf6becae1fe132d03e97561677d948a308582
AndiHSR <andi.hoerler@hsr.ch>
2019-05-15 10:44:23 +0200

    Rename header

--- backend/bin/main/enrichers/threat_info_enricher.py
+++ backend/bin/main/enrichers/threat_info_enricher.py
@@ -13,7 +13,7 @@ from main.helpers.file import file_read_helper
 
 class ThreatInfoEnricher(Enricher):
     def __init__(self):
-        header = "threat_type"
+        header = "threat_category"
         enricher_type = "threat enricher"
         Enricher.__init__(self, enricher_type, header)
 
