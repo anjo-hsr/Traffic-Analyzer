a105b2999fe0dea3470a0f0639264af6efbff3ad
AndiHSR <andi.hoerler@hsr.ch>
2019-05-15 17:04:05 +0200

    Update download.sh

--- backend/bin/download.sh
+++ backend/bin/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SOURCE_FOLDER=$(dirname "${BASH_SOURCE[0]}")
-python3 ${SOURCE_FOLDER}/main/traffic_analyzer.py download &>/dev/null
+python3 ${SOURCE_FOLDER}/main/traffic_analyzer.py download
