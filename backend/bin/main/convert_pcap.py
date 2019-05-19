66fbf3b6d1d17ebf45807f7675a53608a2ca5820
AndiHSR <andi.hoerler@hsr.ch>
2019-05-20 00:34:40 +0200

    Move checking of isFile into extracted methods

--- backend/bin/main/convert_pcap.py
+++ backend/bin/main/convert_pcap.py
@@ -45,15 +45,14 @@ def run(environment_variables) -> None:
     hash_path = path.join(csv_tmp_path, "hashes.txt")
 
     for file_path in file_path_helper.get_file_paths(pcap_path, file_name_helper.is_pcap_file):
-        hash_path_exists = path.isfile(hash_path)
-        file_hashes = file_read_helper.get_file_hashes(hash_path, hash_path_exists)
+        file_hashes = file_read_helper.get_file_hashes(hash_path)
 
         pcap_hash = file_read_helper.get_file_hashsum(path.join(file_path["path"], file_path["filename"]))
         if pcap_hash in file_hashes:
             continue
 
         run_tshark(file_path["filename"], file_path["path"], csv_tmp_path)
-        file_write_helper.write_hash_to_file(hash_path, hash_path_exists, pcap_hash)
+        file_write_helper.write_hash_to_file(hash_path, pcap_hash)
 
 
 if __name__ == "__main__":
