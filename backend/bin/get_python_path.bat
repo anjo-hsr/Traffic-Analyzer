9a6c6a6abd498339bf4c08e8f0aba887b8d5e22d
AndiHSR <andi.hoerler@hsr.ch>
2019-05-25 15:10:19 +0200

    Use pip to install requirements

--- backend/bin/get_python_path.bat
+++ backend/bin/get_python_path.bat
@@ -1,19 +1,24 @@
 @echo off
 
-if exist "C:\Program Files (x86)\Python37-32\python.exe" goto echo32
-if exist "C:\Program Files\Python37-32\python.exe" goto echo64
+if exist "C:\Program Files\Python37-32\python.exe" goto echo32
+if exist "C:\Program Files (x86)\Python37-32\python.exe" goto echo64_32
+if exist "C:\Program Files\Python37\python.exe" goto echo64_64
 
 echo "No python 3.7 installation found in Program Files folders."
+pause
 exit
 
 
 :echo32
-set PYTHON_PATH="C:\Program Files (x86)\Python37-32\python.exe"
+set PYTHON_PATH=C:\Program Files\Python37-32
 goto return
 
+:echo64_32
+set PYTHON_PATH=C:\Program Files (x86)\Python37-32
+goto return
 
-:echo64
-set PYTHON_PATH="C:\Program Files\Python37-32\python.exe"
+:echo64_64
+set PYTHON_PATH=C:\Program Files\Python37
 goto return
 
 
