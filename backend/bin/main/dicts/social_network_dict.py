4902c86b16314b5e3d6d1635f8806de0387f39f0
AndiHSR <andi.hoerler@hsr.ch>
2019-06-03 22:03:17 +0200

    Add dict for social network providers

---
+++ backend/bin/main/dicts/social_network_dict.py
@@ -0,0 +1,128 @@
+# Adapted from https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/social/hosts
+social_network_providers = {
+    "Facebook": [
+        ".facebook.",
+        ".fbcdn.",
+        ".tfbnw.net",
+        ".attachment.fbsbx.com",
+        ".facebook.net.edgekey.net",
+        ".ct-m-fbx.fbsbx.com",
+        ".facebook-web-clients.appspot.com",
+        ".fb.",
+        ".fbcallback.wechat.com",
+        ".fbcdn-creative-a.akamaihd.net",
+        ".fbcdn-photos-a.akamaihd.net",
+        ".fbcdn-photos-e-a.akamaihd.net",
+        ".fbcdn-profile-a.akamaihd.net",
+        ".fbcdn-sphotos-a-a.akamaihd.net",
+        ".fbcdn-sphotos-a.akamaihd.net",
+        ".fbcdn-sphotos-b-a.akamaihd.net",
+        ".fbcdn-sphotos-c-a.akamaihd.net",
+        ".fbcdn-sphotos-d-a.akamaihd.net",
+        ".fbcdn-sphotos-e-a.akamaihd.net",
+        ".fbcdn-sphotos-f-a.akamaihd.net",
+        ".fbcdn-sphotos-g-a.akamaihd.net",
+        ".fbcdn-sphotos-h-a.akamaihd.net",
+        ".fbcdn-video-a-a.akamaihd.net",
+        ".fbcdn-video-b-a.akamaihd.net",
+        ".fbcdn-video-c-a.akamaihd.net",
+        ".fbcdn-video-d-a.akamaihd.net",
+        ".fbcdn-video-e-a.akamaihd.net",
+        ".fbcdn-video-f-a.akamaihd.net",
+        ".fbcdn-video-g-a.akamaihd.net",
+        ".fbcdn-video-h-a.akamaihd.net",
+        ".fbcdn-video-i-a.akamaihd.net",
+        ".fbcdn-video-j-a.akamaihd.net",
+        ".fbcdn-video-k-a.akamaihd.net",
+        ".fbcdn-video-l-a.akamaihd.net",
+        ".fbcdn-video-m-a.akamaihd.net",
+        ".fbcdn-video-n-a.akamaihd.net",
+        ".fbcdn-video-o-a.akamaihd.net",
+        ".fbcdn-video-p-a.akamaihd.net",
+        ".fbcdn-vthumb-a.akamaihd.net",
+        ".fbexternal-a.akamaihd.net",
+        ".fbsbx.com",
+        ".fbstatic-a.akamaihd.net",
+        ".fbstatic.net",
+        ".free.freebasics.com",
+        ".free.internet.org",
+        ".freebasics.com",
+        ".h-ct-m-fbx.fbsbx.com.online-metrix.net",
+        ".internet.org",
+        ".m.me",
+        ".messenger.com",
+        ".platform.foursquare.com",
+        ".facebook.com.edgesuite.net",
+        ".facebook.com.edgekey.net",
+        ".sac-h-ct-m-fbx.fbsbx.com.online-metrix.net",
+        ".zero.freebasics.com",
+        ".internet.org"
+    ],
+
+    "Instagram": [
+        ".instagram.com",
+        ".instagramstatic-a.akamaihd.net",
+        ".instagramstatic-a.akamaihd.net.edgesuite.net"
+    ],
+
+    "Whatsapp": [
+        ".whatsapp.com",
+        ".whatsapp.net",
+    ],
+
+    "Twitter": [
+        ".twitter.com",
+        ".twimg.com",
+        ".t.co",
+    ],
+
+    "LinkedIn": [
+        ".linkedin.com",
+        ".licdn.com",
+    ],
+
+    "MySpace": [
+        ".myspace.com",
+        ".myspacecdn.com",
+    ],
+
+    "Pinterest": [
+        ".pinterest.",
+        ".dualstack.pinterest.map.fastly.net",
+        ".pinimg.com",
+        ".pinimg.com.edgekey.net",
+        ".pin.it",
+        ".pinterest.global.map.fastly.net",
+        ".s.pinimg.com.edgekey.net",
+        ".v.pinimg.com.edgekey.net",
+        ".pinterest.com.au",
+        ".pinterest.com.edgekey.net",
+    ],
+
+    "Tumblr": [
+        ".tumblr.com",
+        ".cookiex.ngd.yahoo.com",
+    ],
+
+    "Google Plus": [
+        ".plus.google.com",
+    ],
+
+    "Reddit": [
+        ".redditmedia.com",
+        ".reddit.com",
+        ".reddit-com.cdn.ampproject.org",
+        ".redd.it",
+        ".reddit-image.s3.amazonaws.com",
+        ".reddit-uploaded-media.s3-accelerate.amazonaws.com",
+        ".reddit-uploaded-video.s3-accelerate.amazonaws.com",
+        ".reddit.map.fastly.net",
+        ".redditgifts.s3.amazonaws.com",
+        ".redditgifts.com",
+        ".redditstatic.com",
+        ".mbga.jp",
+        ".mixi.jp",
+        ".mixch.tv",
+        ".lobi.co",
+    ]
+}
