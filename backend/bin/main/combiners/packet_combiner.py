3c1a8f5b0d965a4a9adbdce60613b6b70f7b66de
AndiHSR <andi.hoerler@hsr.ch>
2019-06-03 10:41:13 +0200

    Use new combined fields

--- backend/bin/main/combiners/packet_combiner.py
+++ backend/bin/main/combiners/packet_combiner.py
@@ -4,10 +4,13 @@ from typing import Dict
 class PacketCombiner:
     @staticmethod
     def get_dst_src(packet) -> Dict[str, str]:
-        dst_src = {"dst": packet["ip.dst"], "src": packet["ip.src"]}
+        dst_src = {
+            "dst": packet["ip_dst_combined"],
+            "src": packet["ip_src_combined"]
+        }
         return dst_src
 
     @staticmethod
     def combine_ip_versions(packet) -> None:
         packet["ip_dst_combined"] = packet["ip.dst"] + packet["ipv6.dst"]
-        packet["ip_dst_combined"] = packet["ip.src"] + packet["ipv6.src"]
+        packet["ip_src_combined"] = packet["ip.src"] + packet["ipv6.src"]
