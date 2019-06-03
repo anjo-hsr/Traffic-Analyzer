3ef38ce5b3a6e2f39fa8a2dfd6a1e27f6d2cd188
AndiHSR <andi.hoerler@hsr.ch>
2019-06-03 10:40:16 +0200

    Add combine fields for IP versions

--- backend/bin/main/combiners/packet_combiner.py
+++ backend/bin/main/combiners/packet_combiner.py
@@ -6,3 +6,8 @@ class PacketCombiner:
     def get_dst_src(packet) -> Dict[str, str]:
         dst_src = {"dst": packet["ip.dst"], "src": packet["ip.src"]}
         return dst_src
+
+    @staticmethod
+    def combine_ip_versions(packet) -> None:
+        packet["ip_dst_combined"] = packet["ip.dst"] + packet["ipv6.dst"]
+        packet["ip_dst_combined"] = packet["ip.src"] + packet["ipv6.src"]
