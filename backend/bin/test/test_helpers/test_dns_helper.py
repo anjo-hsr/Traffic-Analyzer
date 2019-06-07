335d6c253a80539f24fe004760eadccc024c52c2
AndiHSR <andi.hoerler@hsr.ch>
2019-06-07 09:10:37 +0200

    Extract dns lookup methods and add tests

---
+++ backend/bin/test/test_helpers/test_dns_helper.py
@@ -0,0 +1,58 @@
+import unittest
+from unittest.mock import patch, MagicMock, mock_open
+
+from main.helpers.dns_helper import DnsHelper
+
+
+class TestDomainDictHelperMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.dns_helper = DnsHelper()
+
+    @patch("os.path.isfile", MagicMock(return_value=False))
+    def test_set_dns_resolver(self) -> None:
+        dns_lifetime = 2
+        self.dns_helper.set_dns_resolver()
+        self.assertTrue(self.dns_helper.dns_resolver.lifetime, dns_lifetime)
+
+    @patch("os.path.isfile", MagicMock(return_value=True))
+    @patch("main.helpers.file.file_read_helper.open",
+           new=mock_open(read_data="[Stanza]\n" + "internal_dns_server = 8.8.8.8"))
+    def test_set_dns_server(self) -> None:
+        expected_length = len(self.dns_helper.dns_resolver.nameservers) + 1
+        self.dns_helper.set_dns_server()
+        self.assertEqual(len(self.dns_helper.dns_resolver.nameservers), expected_length)
+
+    @patch("os.path.isfile", MagicMock(return_value=True))
+    @patch("main.helpers.file.file_read_helper.open",
+           new=mock_open(read_data="[Stanza]\n" + "internal_dns_server = 1.1.1.1, 8.8.8.8"))
+    def test_set_dns_servers(self) -> None:
+        expected_length = len(self.dns_helper.dns_resolver.nameservers) + 2
+        self.dns_helper.set_dns_server()
+        self.assertEqual(len(self.dns_helper.dns_resolver.nameservers), expected_length)
+
+    @patch("os.path.isfile", MagicMock(return_value=True))
+    @patch("main.helpers.file.file_read_helper.open",
+           new=mock_open(read_data="[Stanza]\n" + "internal_dns_server = 8.8.8.8"))
+    def test_reset_dns_server(self) -> None:
+        expected_length = len(self.dns_helper.dns_resolver.nameservers) + 1
+        self.dns_helper.set_dns_server()
+        self.assertEqual(len(self.dns_helper.dns_resolver.nameservers), expected_length)
+        print(self.dns_helper.dns_resolver.nameservers)
+        expected_length -= 1
+        self.dns_helper.reset_dns_resolver()
+        self.assertEqual(len(self.dns_helper.dns_resolver.nameservers), expected_length)
+        print(self.dns_helper.dns_resolver.nameservers)
+
+    def test_is_dns_server_available_ok(self) -> None:
+        dns_server_address = "8.8.8.8"
+        self.assertTrue(self.dns_helper.is_dns_server_available(dns_server_address))
+
+    def test_is_dns_server_avaiable_not_ok(self) -> None:
+        dns_server_address = "127.0.0.1"
+        self.assertFalse(self.dns_helper.is_dns_server_available(dns_server_address))
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDomainDictHelperMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
