11c45e0afae9b189f9a660533d227fb323ebb6c6
AndiHSR <andi.hoerler@hsr.ch>
2019-08-20 15:51:05 +0200

    Add second line for locally administered addresses

--- backend/bin/main/helpers/string_helper.py
+++ backend/bin/main/helpers/string_helper.py
@@ -8,3 +8,15 @@ def enclose_with_quotes(string_value) -> str:
 
 def remove_spaces(string_value) -> str:
     return string_value.replace(" ", "")
+
+
+def get_mac_address_line(mac_address, organization, locally=False) -> str:
+    from main.combiners.field_combiner import FieldCombiner
+
+    line = mac_address + FieldCombiner.delimiter
+    if locally:
+        line += enclose_with_quotes(organization)
+    else:
+        line += enclose_with_quotes("Locally administered - " + organization)
+
+    return line
