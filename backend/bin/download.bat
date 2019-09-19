2b8b182eed0da151443554021b0c62c2391a1531
AndiHSR <andi.hoerler@hsr.ch>
2019-09-19 12:36:02 +0200

    Use builded traffic_analyzer.exe

--- backend/bin/download.bat
+++ backend/bin/download.bat
@@ -1,7 +1,4 @@
 @echo off
 
 set SOURCE_FOLDER=%~dp0
-call "%SOURCE_FOLDER%\get_python_path.bat"
-
-"%PYTHON_PATH%\Scripts\pip3.7.exe" install -r "%SOURCE_FOLDER%\requirements.txt" > NUL
-"%PYTHON_PATH%\python.exe" -E "%SOURCE_FOLDER%\main\traffic_analyzer.py" download
+call "%SOURCE_FOLDER%\main\traffic_analyzer.exe" download
