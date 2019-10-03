54d9bb5d66f3abcc4a82048734d6520ad4f4b0ba
AndiHSR <andi.hoerler@hsr.ch>
2019-10-03 09:10:48 +0200

    Remove echos from entries

--- app_deployer.sh
+++ app_deployer.sh
@@ -132,15 +132,12 @@ function test_splunk_app() {
 
     tcpEntry=`docker exec ${containerName} bash -c "sudo /opt/splunk/bin/splunk search 'sourcetype=\"list\" Decimal=\"6\" Keyword=\"TCP\"' -auth admin:AnJo-HSR"`
     tcpString="6,TCP,Transmission Control,,[RFC793]"
-    echo -e "${tcpEntry}"
 
     macEntry=`docker exec ${containerName} bash -c "sudo /opt/splunk/bin/splunk search 'sourcetype=\"list\" eth_short=\"00:00:0c\"' -auth admin:AnJo-HSR"`
     macString='00:00:0c,"Cisco Systems, Inc"'
-    echo -e "${macEntry}"
 
     cipherSuiteEntry=`docker exec ${containerName} bash -c "sudo /opt/splunk/bin/splunk search 'sourcetype=\"list\" cipher_suite_number=\"49200\"' -auth admin:AnJo-HSR"`
     cipherSuiteString="49200,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,Y"
-    echo -e "${cipherSuiteEntry}"
 
     if [[ "${trafficAnalyzer}" == *"App '${appName}' is packaged."* ]] && [[ "${tcpEntry}" == "${tcpString}" ]] && [[ "${macEntry}" == "${macString}" ]] && [[ "${cipherSuiteEntry}" == "${cipherSuiteString}" ]]; then
         echo "App test was successful."
